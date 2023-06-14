82aea66c9c9e8a2a11f88b7f6decec4ec36a1758
Daniel Rodriguez <danrodriguez.me@gmail.com>
2023-06-13 20:58:27 -0700

    WIP

--- agency/agents/demo_agent.py
+++ agency/agents/demo_agent.py
@@ -1,21 +1,19 @@
-from datetime import datetime
 from agency import util
 from agency.agent import ACCESS_PERMITTED, access_policy
 from agency.agent import Agent
-from agency.agents.prompt_methods import PromptMethods
-import json
 import openai
 import textwrap
 
 
-class DemoAgent(Agent, PromptMethods):
+class DemoAgent(Agent):
 
     def __init__(self, id, model, openai_api_key, **kwargs):
-        super().__init__(id, **kwargs)
+        super().__init__(id)
         self.__model = model
+        self.__kwargs = kwargs
         openai.api_key = openai_api_key
 
-    def _prompt_head(self):
+    def __system_prompt(self):
         return textwrap.dedent(f"""
         You are "Demo". You are a prototype of an "agent" system which can
         freely interact with its environment.
@@ -29,50 +27,85 @@ class DemoAgent(Agent, PromptMethods):
         your ability to communicate with other systems as needed to solve any
         problems at hand.
 
-        In order to communicate, you use a simple terminal application where
-        your can interact via JSON formatted messages. You can send messages to
-        discover and interact with other systems, AI agents, or humans who may
-        also be present.
+        The following is your current conversation. Respond appropriately.
+        """)
 
-        The following is your current session on the terminal application.
-        Respond with a JSON formatted message to continue the conversation.
+    def __open_ai_messages(self):
+        """
+        Returns a list of messages converted from the message_log to be sent to
+        OpenAI
+        """
+        # start with the system message
+        open_ai_messages = [{ "role": "system", "content": self.__system_prompt() }]
 
-        %%%%% Terminal App 1.0.0 %%%%%
-        (Use the "help" action to see available commands")
-        """) + \
-            self._message_line({
-                "from": self.id(),
-                "to": None,
-                "thoughts": "I should see what commands are available.",
-                "action": "help",
-                "args": {},
-            }, indent=2) + \
-            self._pre_prompt(agent_id=self.space.id()) + \
-            json.dumps(self.space._get_help__sync()) + \
-            self._message_log_to_list()
+        # format and add the rest of the messages
+        # NOTE: openai unfortunately limits us to four predefined chat roles so
+        # we convert to them here. predefined roles aren't the right abstraction
+        # IMHO but this is one way to handle it if we must.
+        for message in self._message_log:
+            # "say" actions are converted to messages using the content arg
+            if message['action'] == "say":
+                # assistant
+                if message["from"] == self.id():
+                    open_ai_messages.append({
+                        "role": "assistant",
+                        "content": message["args"]["content"],
+                    })
+                # user
+                elif message["from"] == self.__kwargs['user_id']:
+                    open_ai_messages.append({
+                        "role": "user",
+                        "content": message["args"]["content"],
+                    })
 
-    def _pre_prompt(self, agent_id, timestamp=util.to_timestamp(datetime.now())):
-        return f"\n[{timestamp}] {agent_id}:"
+                # a "say" from anyone else is considered a function message
+                # NOTE this highlights one of the problems with predefined
+                else:
+                    open_ai_messages.append({
+                        "role": "function",
+                        "content": message["args"]["content"],
+                    })
 
-    def _message_line(self, message, indent=None):
-        pre_prompt = self._pre_prompt(message['from'])
-        return f"{pre_prompt} {json.dumps(message)}/END"
+            # all other action types are considered a function call
+            else:
+                # NOTE Strangely it does not appear that openai suggests
+                # including function_call messages in the messages list. See:
+                # https://github.com/openai/openai-cookbook/blob/main/examples/How_to_call_functions_with_chat_models.ipynb
+                #
+                # I'm not sure why. Instead, I am going to add it here as a
+                # "system" message reporting the details of what the function
+                # call was. This is important information to infer from and it's
+                # currently not clear whether the language model has access to
+                # it from their documentation.
+                open_ai_messages.append({
+                    "role": "system",
+                    "content": f"""{message["from"]} called function "{message["action"]}" with args {message["args"]} and thoughts {message["thoughts"]}""",
+                })
+
+    def __open_ai_functions(self):
+        """
+        Returns a list of functions converted from _get_help__sync() to be sent
+        to OpenAI
+        """
+
 
     @access_policy(ACCESS_PERMITTED)
     def _action__say(self, content: str) -> bool:
-        full_prompt = self._full_prompt()
+        """
+        Sends a message to the openai chatcompletion api, translates the
+        response, and sends the resulting action
+        """
         completion = openai.ChatCompletion.create(
           model=self.__model,
-          messages=[{ "role": "user", "content": full_prompt }],
-          functions=[
-              {
-
-              }
-              for 
-          ]
+          messages=self.__open_ai_messages(),
+          functions=self.__open_ai_functions(),
           # temperature=0.1,
           # max_tokens=500,
         )
+
         # parse the output
+        # TODO: convert to the common message schema here
+        raise NotImplementedError()
+
         action = util.extract_json(completion.choices[0].text, ["/END"])
         self._send(action)
