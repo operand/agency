12b88dc67b2352d9bc0e68fec58d0906c3fe9058
Daniel Rodriguez <307146+operand@users.noreply.github.com>
2023-07-08 18:38:15 -0700

    demo bug workaround (#63)
    temporarily addresses https://github.com/operand/agency/issues/60 while a fix for the library is developed

--- examples/demo/amqp_chatty.py
+++ examples/demo/amqp_chatty.py
@@ -1,11 +1,27 @@
-import time
-from agents.chatty_ai import ChattyAI
 from agency.space import AMQPSpace
+from agents.chatty_ai import ChattyAI
+import os
+import pika
+import time
 
 
 if __name__ == '__main__':
 
-    space = AMQPSpace()
+    # NOTE The following connection parameters are hardcoded to use a heartbeat
+    # of 0. This is to workaround a bug that is currently being worked on. See:
+    # https://github.com/operand/agency/issues/60
+    space = AMQPSpace(
+        pika_connection_params=pika.ConnectionParameters(
+            heartbeat=0,
+            host=os.environ.get('AMQP_HOST', 'localhost'),
+            port=os.environ.get('AMQP_PORT', 5672),
+            virtual_host=os.environ.get('AMQP_VHOST', '/'),
+            credentials=pika.PlainCredentials(
+                os.environ.get('AMQP_USERNAME', 'guest'),
+                os.environ.get('AMQP_PASSWORD', 'guest'),
+            ),
+        )
+    )
 
     # Add a simple HF chat agent to the space
     space.add(
