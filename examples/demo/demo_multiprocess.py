4e0f2db6c0017560251495ab283bda6809aedffb
Daniel Rodriguez <danrodriguez.me@gmail.com>
2023-08-29 23:35:17 -0700

    WIP

--- examples/demo/demo_multiprocess.py
+++ examples/demo/demo_multiprocess.py
@@ -6,33 +6,34 @@ from agents.openai_function_agent import OpenAIFunctionAgent
 from agency.spaces.multiprocess_space import MultiprocessSpace
 from examples.demo.apps.gradio_app import GradioApp
 
-# Create the space instance
-space = MultiprocessSpace()
+if __name__ == "__main__":
 
-# Add a host agent to the space, exposing access to the host system
-space.add(Host, "Host")
+    # Create the space instance
+    space = MultiprocessSpace()
 
-# Add an OpenAI function API agent to the space
-space.add(OpenAIFunctionAgent,
-          "FunctionAI",
-          model="gpt-3.5-turbo-16k",
-          openai_api_key=os.getenv("OPENAI_API_KEY"),
-          # user_id determines the "user" role in the OpenAI chat API
-          user_id="User")
+    # Add a host agent to the space, exposing access to the host system
+    space.add(Host, "Host")
 
-# Add a Gradio app and its user to the space
-space.add(GradioApp, "User")
+    # Add an OpenAI function API agent to the space
+    space.add(OpenAIFunctionAgent,
+              "FunctionAI",
+              model="gpt-3.5-turbo-16k",
+              openai_api_key=os.getenv("OPENAI_API_KEY"),
+              # user_id determines the "user" role in the OpenAI chat API
+              user_id="User")
 
+    # Add a Gradio app and its user to the space
+    space.add(GradioApp, "User")
 
-# Other agents to try (see the ./agents directory)
+    # Other agents to try (see the ./agents directory)
 
-# Add an OpenAI agent based on the completion API
-# space.add(OpenAICompletionAgent,
-#           "CompletionAI",
-#           model="text-davinci-003",
-#           openai_api_key=os.getenv("OPENAI_API_KEY"))
+    # Add an OpenAI agent based on the completion API
+    # space.add(OpenAICompletionAgent,
+    #           "CompletionAI",
+    #           model="text-davinci-003",
+    #           openai_api_key=os.getenv("OPENAI_API_KEY"))
 
-# Add a simple HF chat agent to the space
-# space.add(ChattyAI,
-#           "Chatty",
-#           model="EleutherAI/gpt-neo-125m")
+    # Add a simple HF chat agent to the space
+    # space.add(ChattyAI,
+    #           "Chatty",
+    #           model="EleutherAI/gpt-neo-125m")
--- examples/demo/demo_threaded.py
+++ examples/demo/demo_threaded.py
@@ -6,39 +6,39 @@ from agents.openai_function_agent import OpenAIFunctionAgent
 from agency.spaces.thread_space import ThreadSpace
 from examples.demo.apps.gradio_app import GradioApp
 
-# Create the space instance
-space = ThreadSpace()
+if __name__ == "__main__":
 
-# Add a host agent to the space, exposing access to the host system
-space.add(Host, "Host")
+    # Create the space instance
+    space = ThreadSpace()
 
-# Add an OpenAI function API agent to the space
-space.add(OpenAIFunctionAgent,
-          "FunctionAI",
-          model="gpt-3.5-turbo-16k",
-          openai_api_key=os.getenv("OPENAI_API_KEY"),
-          # user_id determines the "user" role in the OpenAI chat API
-          user_id="User")
+    # Add a host agent to the space, exposing access to the host system
+    space.add(Host, "Host")
 
-# Add the Gradio app and user to the space
-space.add(GradioApp, "User")
+    # Add an OpenAI function API agent to the space
+    space.add(OpenAIFunctionAgent,
+              "FunctionAI",
+              model="gpt-3.5-turbo-16k",
+              openai_api_key=os.getenv("OPENAI_API_KEY"),
+              # user_id determines the "user" role in the OpenAI chat API
+              user_id="User")
 
+    # Add the Gradio app and user to the space
+    space.add(GradioApp, "User")
 
-# Other agents to try
+    # Other agents to try
 
-# Add an OpenAI agent based on the completion API
-# space.add(OpenAICompletionAgent,
-#           "CompletionAI",
-#           model="text-davinci-003",
-#           openai_api_key=os.getenv("OPENAI_API_KEY"))
+    # Add an OpenAI agent based on the completion API
+    # space.add(OpenAICompletionAgent,
+    #           "CompletionAI",
+    #           model="text-davinci-003",
+    #           openai_api_key=os.getenv("OPENAI_API_KEY"))
 
-# Add a simple HF chat agent to the space
-# space.add(ChattyAI,
-#           "Chatty",
-#           model="EleutherAI/gpt-neo-125m")
+    # Add a simple HF chat agent to the space
+    # space.add(ChattyAI,
+    #           "Chatty",
+    #           model="EleutherAI/gpt-neo-125m")
 
-
-# if __name__ == '__main__':
-#     # Importing and launching the Gradio demo from this file allows us to use
-#     # the `gradio` command line for live reloading
-#     demo.launch()
+    # if __name__ == '__main__':
+    #     # Importing and launching the Gradio demo from this file allows us to use
+    #     # the `gradio` command line for live reloading
+    #     demo.launch()
